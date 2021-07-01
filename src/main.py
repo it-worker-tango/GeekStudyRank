5153ed3216e83d81fe458ac17bee7f38f52ec096
Tango <nbh_it@163.com>
2021-07-01 22:48:55 +0800

    获取用户信息

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 class GeekRank():
 
@@ -11,6 +11,13 @@ class GeekRank():
             "Origin": "https://time.geekbang.org"
         }
         self.url = "https://promotion.geekbang.org/race_rank/activity/rank/list"
+        self.rank = None
+        self.uid = None
+        self.medal = None
+        self.score = None
+        self.points = None
+        self.nick_name = None
+        self.user_info = None
 
     def get_data(self):
         req = requests.post(self.url, headers=self.headers, data={})
@@ -20,10 +27,30 @@ class GeekRank():
         return False
 
     def clear_data(self, js_data):
-        print(js_data)
+        info = json.loads(js_data)
+        info_data = info['data']
+        self.user_info = info_data['user_info']
+        return info_data
+
+    def get_nick_name(self, uid):
+        for info in self.user_info:
+            if info['uid'] == uid:
+                return info['nick_name']
+
 
 if __name__ == "__main__":
     geek = GeekRank()
     js_data = geek.get_data()
     if js_data:
-        geek.clear_data(js_data)
+        info_data = geek.clear_data(js_data)
+
+        for rank in info_data['list']:
+            rank_index = rank['rank']
+            uid = rank['uid']
+            medal = rank['medal']
+            score = rank['score']
+            points = rank['points']
+            
+            nick_name = geek.get_nick_name(uid)
+            print(f"{nick_name}:{rank_index}:{points}")
+            print("-" * 50)
